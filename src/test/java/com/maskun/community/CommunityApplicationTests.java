e4b0be7140b3349c047c82bd9517a037d948b51c
MASKUN2 <maskunflower2@naver.com>
2024-05-25 22:33:28 +0900

    Initial commit

---
+++ src/test/java/com/maskun/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maskun.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
